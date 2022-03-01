47feb98d2b23f396e160f513c1ef263de7611df2
Nico Huettmann <huttmann.nico@gmail.com>
2022-03-01 16:57:34 +0100

    Add new order

--- _render.R
+++ _render.R
@@ -4,15 +4,15 @@
 bookdown::render_book(input = "index.Rmd",
                       output_format = "bookdown::gitbook",
                       config_file = "_bookdown.yml",
-                      preview = TRUE)
+                      preview = FALSE)
 
 
 
 
-render_book(input = ".", output_format = NULL, ..., clean = TRUE,
-            envir = parent.frame(), clean_envir = !interactive(),
-            output_dir = NULL, new_session = NA, preview = FALSE,
-            config_file = "_bookdown.yml")
+# render_book(input = ".", output_format = NULL, ..., clean = TRUE,
+#             envir = parent.frame(), clean_envir = !interactive(),
+#             output_dir = NULL, new_session = NA, preview = FALSE,
+#             config_file = "_bookdown.yml")
 
 
 
